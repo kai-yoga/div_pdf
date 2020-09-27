e3f6434909b3b29cc6727481c58e2482900bea6d
80662 <1532398723@qq.com>
2020-09-27 18:49:42 +0800

    --init

--- process/readpdf.py
+++ process/readpdf.py
@@ -5,25 +5,29 @@ import openpyxl as opl
 import time
 def get_dic(file_name):
     '''识别拆分文件页码范围'''
-    text=extract_text(file_name,password='Gzdx230!@#$')
-    text=repr(text).replace('\n','').split('广州大学')
-    L=[]
-    dic={}
-    for i in range(len(text)):
-        if len(text[i])>5:
-            # print(i,text[i])
-            xh=text[i].split('学 号')[-1].split('姓 名')[0].replace(' ','').replace('\\n','')
-            xm=text[i].split('姓 名')[-1].split('年 级')[0].replace(' ','').replace('\\n','')
-            nj=text[i].split('年 级')[-1].split('班 级')[0].replace(' ','').replace('\\n','')
-            bj=text[i].split('班 级')[-1].split('\\n')[2].replace(' ','').replace('\\n','')
-            lsh=text[i].split('打印流水号:')[-1].split('#')[0]
-            if bj=='性质':
-                print(text[i])
-                # print(xh,xm,nj,bj,lsh)
-            L.append(xh+'#'+xm+'#'+nj+'#'+bj+'#'+lsh)
-    for index in range(len(L)):
-        if L[index] not in dic.keys():
-            dic[L[index]]=str(L.index(L[index]))+'-'+str(index+L.count(L[index])-1)
+    L = []
+    dic = {}
+    try:
+        text=extract_text(file_name,password='Gzdx230!@#$')
+        text=repr(text).replace('\n','').split('广州大学')
+
+        for i in range(len(text)):
+            if len(text[i])>5:
+                # print(i,text[i])
+                xh=text[i].split('学 号')[-1].split('姓 名')[0].replace(' ','').replace('\\n','')
+                xm=text[i].split('姓 名')[-1].split('年 级')[0].replace(' ','').replace('\\n','')
+                nj=text[i].split('年 级')[-1].split('班 级')[0].replace(' ','').replace('\\n','')
+                bj=text[i].split('班 级')[-1].split('\\n')[2].replace(' ','').replace('\\n','')
+                lsh=text[i].split('打印流水号:')[-1].split('#')[0]
+                if bj=='性质':
+                    print(text[i])
+                    # print(xh,xm,nj,bj,lsh)
+                L.append(xh+'#'+xm+'#'+nj+'#'+bj+'#'+lsh)
+        for index in range(len(L)):
+            if L[index] not in dic.keys():
+                dic[L[index]]=str(L.index(L[index]))+'-'+str(index+L.count(L[index])-1)
+    except:
+        print('该pdf文件不是标准的学年学生成绩单！')
     return dic
 #'1401100011#王燕萍#2016#地理161#06132022': '0-0',
 # def mkdirs(savepath,dic={}):
